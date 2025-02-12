d92e0cef8670a834d890cd26424ce172eef552f9
techno123f <154799042+techno123f@users.noreply.github.com>
2025-02-12 13:00:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.querySelector(".menu-btn");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
